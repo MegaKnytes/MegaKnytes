4a3e78073c3fd5584c63b32ec70166cb92b60d9b
UntitledMoose <noah@untitledmoose.dev>
2024-09-30 01:00:57 -0500

    Device/Driver Loading & Setup (working)

--- MegaKnytes/src/main/java/com/MegaKnytes/DecisionTable/DTProcessor.java
+++ MegaKnytes/src/main/java/com/MegaKnytes/DecisionTable/DTProcessor.java
@@ -1,5 +1,37 @@
 package com.MegaKnytes.DecisionTable;
 
-public class DTProcessor {
+import android.content.Context;
 
+import com.MegaKnytes.DecisionTable.drivers.DTDriverRegistry;
+import com.MegaKnytes.DecisionTable.drivers.DTPDriver;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+import java.io.File;
+import java.util.HashMap;
+
+public class DTProcessor {
+    private HashMap<String, Class<? extends DTPDriver>> driverClassList = new HashMap<>();
+    private final HardwareMap hwMap;
+
+    public DTProcessor(HardwareMap hwMap){
+        this.hwMap = hwMap;
+        updateDriverList(hwMap.appContext);
+    }
+
+    public void updateDriverList(Context context){
+        driverClassList = DTDriverRegistry.getClassesWithInstanceOf(context, DTPDriver.class);
+    }
+
+    public HashMap<String, Class<? extends DTPDriver>> getDriverList(){
+        return driverClassList;
+    }
+
+    public HashMap<String, Class<? extends DTPDriver>> getDriverClassList(){
+        return driverClassList;
+    }
+
+    public File getFileFromApplicationContext(String fileName){
+        return hwMap.appContext.getFileStreamPath(fileName);
+    }
 }
--- MegaKnytes/src/main/java/com/MegaKnytes/DecisionTable/drivers/DTDriverRegistry.java
+++ MegaKnytes/src/main/java/com/MegaKnytes/DecisionTable/drivers/DTDriverRegistry.java
@@ -2,9 +2,13 @@ package com.MegaKnytes.DecisionTable.drivers;
 
 import android.content.Context;
 
+import com.MegaKnytes.DecisionTable.DTProcessor;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,6 +18,14 @@ import dalvik.system.DexFile;
 public class DTDriverRegistry {
 
     private static final Logger LOGGER = Logger.getLogger(DTDriverRegistry.class.getName());
+    private static final List<String> IGNORED_PACKAGES = new ArrayList<>(Arrays.asList(
+            "java",
+            "android",
+            "com.sun",
+            "com.vuforia",
+            "com.google",
+            "kotlin"
+    ));
 
     public static HashMap<String, Class<? extends DTPDriver>> getClassesWithInstanceOf(Context context, Class<? extends DTPDriver> classObject) {
         HashMap<String, Class<? extends DTPDriver>> driverClasses = new HashMap<>();
@@ -21,15 +33,25 @@ public class DTDriverRegistry {
         try {
             DexFile dexFile = new DexFile(context.getPackageCodePath());
             for (String className : Collections.list(dexFile.entries())) {
+                boolean skip = false;
+                for (String ignoredPackage : IGNORED_PACKAGES){
+                    if (className.startsWith(ignoredPackage)){
+                        skip = true;
+                        break;
+                    }
+                }
+                if (skip){
+                    continue;
+                }
+
                 try {
-                    Class<?> configClass = Class.forName(className, false, DTDriverRegistry.class.getClassLoader());
+                    Class<?> configClass = Class.forName(className, false, DTProcessor.class.getClassLoader());
                     if (Arrays.asList(configClass.getInterfaces()).contains(DTPDriver.class)) {
                         //TODO: Fix Unchecked Cast - Working for now
                         Class<? extends DTPDriver> driverClass = (Class<? extends DTPDriver>) configClass;
                         driverClasses.put(configClass.getSimpleName(), driverClass);
                     }
-                } catch (ClassNotFoundException e) {
-                    LOGGER.log(Level.SEVERE, "Error while attempting to setup class", e);
+                } catch (ClassNotFoundException | NoClassDefFoundError ignored) {
                 }
             }
         } catch (Exception e) {
